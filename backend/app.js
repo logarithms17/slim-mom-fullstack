7bb1d67c330384f83092cd335fa1619110dd3c23
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-02 23:22:54 +0800

    added folders

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ import cors from "cors"
 
 const app = express()
 
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
@@ -12,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (_req, res) => {
-  res.send('Backend is working!!');
+  res.send('Backend is working!!!');
 });
 
 // 404 Error Handler

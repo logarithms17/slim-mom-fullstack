b0bb67c2d296f5d81e6173e3022d34c9d967ac38
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-03 15:21:48 +0800

    update basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/slim-mom-fullstack'>
       <App />
     </BrowserRouter>
   </React.StrictMode>

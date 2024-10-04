620b0200324abbd7538747274dd09b57b1732800
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-04 11:14:32 +0800

    added mongoose schema for users

---
+++ backend/models/usersModel.js
@@ -0,0 +1,20 @@
+import { Schema } from 'mongoose';
+import mongoose from 'mongoose';
+
+const usersSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+});
+
+export const Users = mongoose.model('users', usersSchema);

db2f13b58c221460a0fa8f6d101b795838b76ba9
d3monw1ngs <tezzielumsit87@gmail.com>
2024-10-04 20:45:41 +0800

    initial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-import { LoginForm } from '../components/LoginForm/LoginForm'
+import { RegisterForm } from 'components/RegisterForm/RegisterForm'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>HomePage
-    <LoginForm />
+    <RegisterForm />
     </div>
   )
 }

b8bbd8b249ff3855c0d6ab51f0504942f3f8bb67
rlmgtr <rlmagtira@gmail.com>
2024-10-06 13:09:14 +0800

    prepared and test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import css from '../components/PageStyles/HomePageStyle.css'
 
 const HomePage = () => {
   return (
     <div>
     <h2>Calculate your daily calorie intake right now</h2>
+<h3 className='testonly'>this is css test</h3>
+   
 
 
 

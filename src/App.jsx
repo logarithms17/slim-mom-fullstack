aa94768ea736fbeb3e83307c39d71f7c37aeb0fd
valeeriiiee <obleavalerie@gmail.com>
2024-10-06 20:14:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" component={RegistrationPage} />
+        <Route path="/register" element={RegistrationPage} />
       </Route>
     </Routes>
   );

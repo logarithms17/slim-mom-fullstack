24e89f30c5b6617fdc2ed7dbf806ec8753e30e9e
rlmgtr <rlmagtira@gmail.com>
2024-10-06 17:29:32 +0800

    updated
    duplicate work - will handle another backlog

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,26 +7,7 @@ const HomePage = () => {
 
 
 
-<div className='calculateForm'>
-  <heading>
-  <h2 className='heading'>Calculate your daily calorie intake right now</h2>
-</heading>
-
-<input name='heightInput' className='inputStyle' placeholder='Height*'/>    
-
-<input name='desiredWeightInput' className='inputStyle' placeholder='Desired Weight*'/>  
-
-<input name='ageInput' className='inputStyle' placeholder='Age*'/>  
-
-<input name='bloodTypeInput' className='inputStyle' placeholder='Blood Type*'/>  
-
-<input name='currentWeightInput' className='inputStyle' placeholder='Current Weight*'/>  
-
-
 </div>
-
-
-    </div>
   )
 }
 

1b00db6bb09ce31520674806500673395440bd3d
olsen1229 <marylou.olsen7@gmail.com>
2024-10-09 19:53:39 +0800

    commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,70 +1,12 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import { LoginForm } from 'components/LoginForm/loginForm';
-import bcrypt from "bcryptjs/dist/bcrypt";
-
+import React from 'react';
+import { LoginForm } from '../components/LoginForm/LoginForm';
 
 const LoginPage = () => {
   return (
-    <div><LoginForm/></div>
+    <div>
+      <LoginForm />
+    </div>
   )
-  
-=======
-import React from "react";
-
-const LoginPage = () => {
-  return (
-    <div>LoginForm</div>
-  )
-    
-   
-
-
->>>>>>> Stashed changes
 }
-const loginUser = async (req, res) => {
-  try {
-      const { email, password } = req.body
 
-<<<<<<< Updated upstream
-      const existingUser = await User.findOne({ email });
-
-    
-      if (!existingUser) {
-          return res.status(401).json({ message: "Email or password is wrong" });
-        }
-        
-        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-
-    
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Password is wrong. Click forgot password to reset." });
-        }
-        
-        const payload = { id: existingUser._id };
-        
-        const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23" });
-
-        await User.findByIdAndUpdate(existingUser._id, { token: token });
-        res.status(200).json({
-            token: token,
-            user: {
-                email: existingUser.email,
-                subscription: user.subscription,
-               
-            },
-            
-        });
-
-              
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-   
-    }
-   
-};
-
-
-=======
->>>>>>> Stashed changes
-export default LoginPage;
+export default LoginPage

e86ebe1ccc8f3f17aa4e3c508ad7a62b8aec3e9a
valeeriiiee <obleavalerie@gmail.com>
2024-10-10 21:39:46 +0800

    Update CalculatorPage.jsx

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import DailyCaloriesForm from '../components/DailyCaloriesForm/DailyCaloriesForm';
 import Summary from '../components/Summary/Summary';
 import style from './CalculatorPage.module.css';
 
 const CalculatorPage = () => {
-  //   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  // const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
-  //   if (!isLoggedIn) {
-  //     return <Navigate to="/home" />;
-  //   }
+  // if (!isLoggedIn) {
+  //   return <Navigate to="/login" />;
+  // }
 
   return (
     <div className={style.container}>

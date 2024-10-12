c0da311661198aad5a16df84c74ba3d905e1786d
rlmgtr <rlmagtira@gmail.com>
2024-10-12 14:55:45 +0800

    added modal
    Height conversion

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import styles from '../components/PageStyles/HomePageStyle.css'
+import React, {useState} from 'react';
+import heightConversionImage from '../components/images/measurementReference/heightConversion.jpg';
+import styles from '../components/PageStyles/Modal-Style.css';
+
 
 const HomePage = () => {
+const [isModalOpen, setIsModalOpen] = useState(false);
+
+const openModal = () => {
+  setIsModalOpen(true);
+};
+
+const closeModal = () => {
+  setIsModalOpen(false);
+};
+  
   return (
 <div>
+Need reference for your height measurement? 
+Click <a href="#!" onClick={openModal}>
+  here
+</a>
+
+{isModalOpen && (
+<div className='modalHeight'>
+  <div className='modalHeight-Content'>
+    <span className='close' onClick={closeModal}>
+      Close
+    </span>
+<img src={heightConversionImage} alt="Height Measurement Reference"/>
+  </div>
+</div>
+)}
+
+
 
 
 

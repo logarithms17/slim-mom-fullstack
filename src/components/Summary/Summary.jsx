f70d250a3f5a69a0c6fc370386f6a9678990be23
valeeriiiee <obleavalerie@gmail.com>
2024-10-15 07:39:23 +0800

    Update Summary.jsx

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import style from './Summary.module.css';
 
 axios.defaults.baseURL = 'https://slim-mom-fullstack.onrender.com';
 
@@ -54,63 +55,75 @@ export const Summary = () => {
   }, 0);
 
   return (
-    <div className="summary">
-      <h2>Summary for {specificDate}</h2>
-      <p>
-        Left{' '}
-        {isNaN(userData?.user?.usersInfo?.recommendedCalories - totalCalories)
-          ? '000'
-          : String(
-              Math.round(
-                userData?.user?.usersInfo?.recommendedCalories - totalCalories
-              ) // Round off
-            ).padStart(3, '0')}{' '}
-        kcal
-      </p>
-      <p>
-        Consumed{' '}
-        {isNaN(totalCalories)
-          ? '000'
-          : String(Math.round(totalCalories)).padStart(3, '0')}{' '}
-      </p>
-      <p>
-        Daily rate:{' '}
-        {isNaN(userData?.user?.usersInfo?.recommendedCalories)
-          ? '000'
-          : String(
-              Math.round(userData?.user?.usersInfo?.recommendedCalories || 0)
-            ).padStart(3, '0')}{' '}
-      </p>
-      <p>
-        {totalCalories > 0 && userData?.user?.usersInfo?.recommendedCalories > 0
-          ? String(
-              Math.round(
-                (totalCalories / userData.user.usersInfo.recommendedCalories) *
-                  100
-              )
-            ).padStart(3, '0')
-          : '000'}
-        % of normal xxx %
-      </p>
-      <h3>Food not recommended</h3>
-      <ul>
-        {userData?.user?.usersInfo?.foodsNotRecommended.map((food, index) => (
-          <li key={index}>{food}</li>
-        ))}
-      </ul>
-      <h3>Consumed Products</h3>
-      <ul>
-        {consumedProducts.length > 0 ? (
-          consumedProducts.map((product, index) => (
-            <li key={index}>
-              {product.product} -{' '}
-              {String(Math.round(product.calories)).padStart(3, '0')} kcal
-            </li>
-          ))
+    <div className={style.summaryContainer}>
+      <div classname={style.summarySection}>
+        <h2>Summary for {specificDate}</h2>
+        <p>
+          Left{' '}
+          {isNaN(userData?.user?.usersInfo?.recommendedCalories - totalCalories)
+            ? '000'
+            : String(
+                Math.round(
+                  userData?.user?.usersInfo?.recommendedCalories - totalCalories
+                ) // Round off
+              ).padStart(3, '0')}{' '}
+          kcal
+        </p>
+        <p>
+          Consumed{' '}
+          {isNaN(totalCalories)
+            ? '000'
+            : String(Math.round(totalCalories)).padStart(3, '0')}{' '}
+        </p>
+        <p>
+          Daily rate:{' '}
+          {isNaN(userData?.user?.usersInfo?.recommendedCalories)
+            ? '000'
+            : String(
+                Math.round(userData?.user?.usersInfo?.recommendedCalories || 0)
+              ).padStart(3, '0')}{' '}
+        </p>
+        <p>
+          {totalCalories > 0 &&
+          userData?.user?.usersInfo?.recommendedCalories > 0
+            ? String(
+                Math.round(
+                  (totalCalories /
+                    userData.user.usersInfo.recommendedCalories) *
+                    100
+                )
+              ).padStart(3, '0')
+            : '000'}
+          % of normal xxx %
+        </p>
+      </div>
+      <div className={style.foodsNotRecommendedSection}>
+        <h3>Food not recommended</h3>
+        {userData?.user?.usersInfo?.foodsNotRecommended?.length > 0 ? (
+          <ul>
+            {userData.user.usersInfo.foodsNotRecommended.map((food, index) => (
+              <li key={index}>{food}</li>
+            ))}
+          </ul>
         ) : (
-          <li>No products consumed on this date.</li>
+          <li>Your diet will be displayed here</li>
         )}
-      </ul>
+      </div>
+      <div classname={style.consumedProductsSection}>
+        <h3>Consumed Products</h3>
+        <ul>
+          {consumedProducts.length > 0 ? (
+            consumedProducts.map((product, index) => (
+              <li key={index}>
+                {product.product} -{' '}
+                {String(Math.round(product.calories)).padStart(3, '0')} kcal
+              </li>
+            ))
+          ) : (
+            <li>No products consumed on this date.</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };

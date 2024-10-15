7e7cd20db9c55f24670ef2d9e41d1d3b8ef56ca9
sherwinmanosa <sherwinmanosa825@gmail.com>
2024-10-15 21:25:05 +0800

    rev18

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import style from './summary.module.css';
 
 axios.defaults.baseURL = 'https://slim-mom-fullstack.onrender.com';
 
-export const Summary = () => {
+export const Summary = ({ selectedDate }) => {
   const [userData, setUserData] = useState(null);
   const [consumedProducts, setConsumedProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const specificDate = '2024-10-14';
+  //DATE WILL BE CURRENT DATE
+  const date = selectedDate.toISOString().slice(0, 10);
+
+  console.log('summary rerendered');
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -23,7 +27,7 @@ export const Summary = () => {
           const userData = response.data;
           setUserData(userData);
 
-          return axios.get(`/api/products/getConsumedProduct/${specificDate}`);
+          return axios.get(`/api/products/getConsumedProduct/${date}`);
         })
         .then(response => {
           const products = response.data.dailyConsumedProducts;
@@ -39,7 +43,7 @@ export const Summary = () => {
     } else {
       setLoading(false);
     }
-  }, [specificDate]);
+  }, [date]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -49,68 +53,72 @@ export const Summary = () => {
     return <div style={{ color: 'red' }}>{error}</div>;
   }
 
+  // Utility function to capitalize the first letter
+  const capitalizeFirstLetter = string =>
+    string ? string.charAt(0).toUpperCase() + string.slice(1) : '';
+
   const totalCalories = consumedProducts.reduce((total, product) => {
     return total + (product.calories || 0);
   }, 0);
 
   return (
-    <div className="summary">
-      <h2>Summary for {specificDate}</h2>
-      <p>
-        Left{' '}
-        {isNaN(userData?.user?.usersInfo?.recommendedCalories - totalCalories)
-          ? '000'
-          : String(
-              Math.round(
-                userData?.user?.usersInfo?.recommendedCalories - totalCalories
-              ) // Round off
-            ).padStart(3, '0')}{' '}
-        kcal
-      </p>
-      <p>
-        Consumed{' '}
-        {isNaN(totalCalories)
-          ? '000'
-          : String(Math.round(totalCalories)).padStart(3, '0')}{' '}
-      </p>
-      <p>
-        Daily rate:{' '}
-        {isNaN(userData?.user?.usersInfo?.recommendedCalories)
-          ? '000'
-          : String(
-              Math.round(userData?.user?.usersInfo?.recommendedCalories || 0)
-            ).padStart(3, '0')}{' '}
-      </p>
-      <p>
-        {totalCalories > 0 && userData?.user?.usersInfo?.recommendedCalories > 0
-          ? String(
-              Math.round(
-                (totalCalories / userData.user.usersInfo.recommendedCalories) *
-                  100
-              )
-            ).padStart(3, '0')
-          : '000'}
-        % of normal xxx %
-      </p>
-      <h3>Food not recommended</h3>
-      <ul>
-        {userData?.user?.usersInfo?.foodsNotRecommended.map((food, index) => (
-          <li key={index}>{food}</li>
-        ))}
-      </ul>
-      <h3>Consumed Products</h3>
-      <ul>
-        {consumedProducts.length > 0 ? (
-          consumedProducts.map((product, index) => (
-            <li key={index}>
-              {product.product} -{' '}
-              {String(Math.round(product.calories)).padStart(3, '0')} kcal
-            </li>
-          ))
+    <div className={style.summaryContainer}>
+      <div className={style.summarySection}>
+        <h2 className={style.summaryContainerHeader}>Summary for {date}</h2>
+        <p>
+          Left{' '}
+          {isNaN(userData?.user?.usersInfo?.recommendedCalories - totalCalories)
+            ? '000'
+            : String(
+                Math.round(
+                  userData?.user?.usersInfo?.recommendedCalories - totalCalories
+                )
+              ).padStart(3, '0')}{' '}
+          kcal
+        </p>
+        <p>
+          Consumed{' '}
+          {isNaN(totalCalories)
+            ? '000'
+            : String(Math.round(totalCalories)).padStart(3, '0')}{' '}
+          kcal
+        </p>
+        <p>
+          Daily rate{' '}
+          {isNaN(userData?.user?.usersInfo?.recommendedCalories)
+            ? '000'
+            : String(
+                Math.round(userData?.user?.usersInfo?.recommendedCalories || 0)
+              ).padStart(3, '0')}{' '}
+          kcal
+        </p>
+        <p>
+          % of normal{' '}
+          {totalCalories > 0 &&
+          userData?.user?.usersInfo?.recommendedCalories > 0
+            ? String(
+                Math.round(
+                  (totalCalories /
+                    userData.user.usersInfo.recommendedCalories) *
+                    100
+                )
+              ).padStart(3, '0')
+            : '000'}{' '}
+          kcal
+        </p>
+      </div>
+      <div className={style.foodsNotRecommendedSection}>
+        <h3 className={style.summaryContainerHeader}>Food not recommended</h3>
+        {userData?.user?.usersInfo?.foodsNotRecommended?.length > 0 ? (
+          <ul>
+            {userData.user.usersInfo.foodsNotRecommended.map((food, index) => (
+              <li key={index}>{capitalizeFirstLetter(food)}</li>
+            ))}
+          </ul>
         ) : (
-          <li>No products consumed on this date.</li>
+          <li>Your diet will be displayed here</li>
         )}
-      </ul>
+      </div>
     </div>
   );
 };

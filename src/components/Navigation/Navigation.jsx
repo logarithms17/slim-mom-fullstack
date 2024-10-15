89a4916607da2824e041bf2aa1f6f72b279b77a3
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-15 23:22:41 +0800

    update nav css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,39 @@
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import css from './Navigation.module.css';
 import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 import HamburgerMenu from 'components/hamburger/Hamburger';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://slim-mom-fullstack.onrender.com';
 
 export default function Navigation() {
+  //fetch user data
+  const [userData, setUserData] = useState('');
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        // Assuming the token is stored in localStorage
+        const token = localStorage.getItem('token');
+        const response = await axios.get('/api/users/getUserData', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUserData(response.data.user.name);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <header className={css.header}>
-      
       <div className={css.container}>
-      <HamburgerMenu/>
+        <HamburgerMenu />
         <img src={logo} alt="slim mom logo"></img>
 
         <div className={css.dividerOne}></div>
@@ -34,14 +57,13 @@ export default function Navigation() {
         </nav>
 
         <div className={css.userExit}>
-        <p className={css.userName}></p>
+          <p className={css.userName}>{userData}</p>
           <div className={css.divider}></div>
           <NavLink to="/login" className={css.exit}>
             Exit
           </NavLink>
         </div>
       </div>
-     
     </header>
   );
 }

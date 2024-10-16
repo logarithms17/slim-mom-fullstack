9c0cf6eea7859ab7553a2a90a9add127f67ba8aa
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-16 19:28:07 +0800

    Revert "Merge branch 'Gerald-branch' into Bryan-branch"
    This reverts commit 261d5031e9320e75ab523d54713a01252ab82eea, reversing
    changes made to 59678959d2ccb2009975bddfd3dfe54e993d18e3.

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from 'react';
-import 'react-datepicker/dist/react-datepicker.css';
-import { ReactComponent as CalendarIcon } from '../../assets/images/calendar 1.svg';
-import DatePicker from 'react-datepicker';
+import React from 'react';
 import css from './calendar.module.css';
 
 const Calendar = ({ selectedDate, onDateChange }) => {
@@ -11,45 +8,14 @@ const Calendar = ({ selectedDate, onDateChange }) => {
     onDateChange(newDate);
   };
 
-  const handleDateChange = date => {
-    setStartDate(date);
-    setIsOpen(false);
-  };
-
-  console.log(startDate);
-
-  const formatDate = date => {
-    if (!date) return '';
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    return `${day}.${month}.${year}`;
-  };
-
-  const formattedDate = formatDate(startDate);
-
   return (
-    <div className={css.appContainer}>
-      <p className={css.selectedDate}>{formattedDate}</p>
-      <div className={css.datePickerContainer}>
-        <CalendarIcon
-          width="30"
-          height="30"
-          className={css.calendarButton}
-          onClick={handleCalendarClick}
-        />
-        {isOpen && (
-          <div className={css.datePickerWrapper}>
-            <DatePicker
-              selected={startDate}
-              onChange={handleDateChange}
-              inline
-              onClickOutside={() => setIsOpen(false)}
-              className={css.datePicker}
-            />
-          </div>
-        )}
-      </div>
-    </div>
+    <input
+      type="date"
+      value={selectedDate.toISOString().substring(0, 10)}
+      onChange={handleChange}
+      className={css.dateInputField}
+    />
   );
-}
+};
+
+export default Calendar;

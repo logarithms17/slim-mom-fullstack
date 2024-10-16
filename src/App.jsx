7d1cdbd00f33296c6df9e880dbe5459d8277f696
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-16 21:51:54 +0800

    added react lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import HomePage from 'pages/HomePage/HomePage';
-import LoginPage from 'pages/LoginPage/LoginPage';
-import DiaryPage from 'pages/DiaryPage/DiaryPage';
-import CalculatorPage from 'pages/CalculatorPage/CalculatorPage';
-import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
-import SharedLayout from 'components/SharedLayout';
+import React, { useState, Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import SharedLayout from 'components/SharedLayout';
 import { PrivateRoute } from 'components/PrivateRoute/PrivateRoute';
 import { RestrictedRoute } from 'components/RestrictedRoute/RestrictedRoute';
 
-import { useState } from 'react';
+// Lazy loading components
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+const DiaryPage = lazy(() => import('pages/DiaryPage/DiaryPage'));
+const CalculatorPage = lazy(() =>
+  import('pages/CalculatorPage/CalculatorPage')
+);
+const RegistrationPage = lazy(() =>
+  import('pages/RegistrationPage/RegistrationPage')
+);
 
 export const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // New state to track login status
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
@@ -30,58 +35,60 @@ export const App = () => {
         overflowY: 'scroll',
       }}
     >
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <SharedLayout isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-          }
-        >
-          {/* Restricted Routes */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
           <Route
             path="/"
             element={
-              <RestrictedRoute redirectTo="/calculator">
-                <HomePage />
-              </RestrictedRoute>
+              <SharedLayout isLoggedIn={isLoggedIn} onLogout={handleLogout} />
             }
-          />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute redirectTo="/calculator">
-                <RegistrationPage />
-              </RestrictedRoute>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute redirectTo="/calculator">
-                <LoginPage onLogin={handleLogin} />
-              </RestrictedRoute>
-            }
-          />
+          >
+            {/* Restricted Routes */}
+            <Route
+              path="/"
+              element={
+                <RestrictedRoute redirectTo="/calculator">
+                  <HomePage />
+                </RestrictedRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute redirectTo="/calculator">
+                  <RegistrationPage />
+                </RestrictedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute redirectTo="/calculator">
+                  <LoginPage onLogin={handleLogin} />
+                </RestrictedRoute>
+              }
+            />
 
-          {/* Pivate Routes: Only accessible to logged in users */}
-          <Route
-            path="/diary"
-            element={
-              <PrivateRoute redirectTo="/login">
-                <DiaryPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/calculator"
-            element={
-              <PrivateRoute redirectTo="/login">
-                <CalculatorPage />
-              </PrivateRoute>
-            }
-          />
-        </Route>
-      </Routes>
+            {/* Private Routes: Only accessible to logged-in users */}
+            <Route
+              path="/diary"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <DiaryPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/calculator"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <CalculatorPage />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
